2907d29c4931cbe9c67bf43359df70321efbbeee
loca1h0st <chenjh.network@gmail.com>
2024-01-05 10:35:08 +0800

    add login function

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,7 +44,8 @@
                     @csrf
                     <div class="mb-3">
                         <label class="form-label">Email address</label>
-                        <input type="email" class="form-control" name="email" placeholder="your@email.com" autocomplete="off">
+                        <input type="email" class="form-control" name="email" placeholder="your@email.com"
+                               autocomplete="off">
                     </div>
                     <div class="mb-2">
                         <label class="form-label">
@@ -54,10 +55,15 @@
                   </span>
                         </label>
                         <div class="input-group input-group-flat">
-                            <input type="password" class="form-control" name="password"  placeholder="Your password"  autocomplete="off">
+                            <input type="password" class="form-control" name="password" placeholder="Your password"
+                                   autocomplete="off">
                             <span class="input-group-text">
                     <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                           stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                           stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path
+                                  d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"/><path
+                                  d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"/></svg>
                     </a>
                   </span>
                         </div>
@@ -75,7 +81,7 @@
             </div>
         </div>
         <div class="text-center text-muted mt-3">
-            Don't have account yet? <a href="./sign-up.html" tabindex="-1">Sign up</a>
+            Don't have account yet? <a href="{{ route('register') }}" tabindex="-1">Sign up</a>
         </div>
     </div>
 </div>
