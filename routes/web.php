c01b67b7db4c6e04be80ba0df09d645d901b7e08
loca1h0st <chenjh.network@gmail.com>
2024-01-05 10:00:58 +0800

    add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
-});
+})->middleware('auth');
 
 Route::get('/login', function () {
     return view('auth.login');
-});
+})->name('login');
 
 Route::get('/forgot-password', function () {
     return view('auth.forgot-password');
-});
+})->name('forgot-password');
