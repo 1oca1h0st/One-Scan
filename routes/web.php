620618d74a0f4ce4edaf18c14a86c64baae82620
loca1h0st <chenjh.network@gmail.com>
2024-01-05 10:10:37 +0800

    add register view & route

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
 Route::get('/forgot-password', function () {
     return view('auth.forgot-password');
 })->name('forgot-password');
